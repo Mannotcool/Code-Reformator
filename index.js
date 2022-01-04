e43a912796c4a5a5706b84bc4e9dfea17bc92410
Nick <nickstoyanov2019@gmail.com>
2022-01-04 14:11:05 -0500

    Replace the haste-bin library.

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const { Client } = require('discord.js');
 const client = new Client({ intents: 32509 });
 const config = require('./config.json');
-const hastebin = require("hastebin");
+const hastebin = require('better-hastebin');
 var detectLang = require('lang-detector');
+
+let options = {
+    raw: false
+};
+  
 
 client.on('ready', async () => {
     console.log('Logged in to Discord');
@@ -13,14 +18,8 @@ client.on("messageCreate", async (message) => {
         if(message.mentions.everyone || message.mentions.here) return;
         if (detectLang(message.content) === 'JavaScript' || detectLang(message.content) === 'C' || detectLang(message.content) === 'C++' || detectLang(message.content) === 'Python' || detectLang(message.content) === 'Java' || detectLang(message.content) === 'HTML' || detectLang(message.content) === 'CSS' || detectLang(message.content) === 'Ruby' || detectLang(message.content) === 'Go' || detectLang(message.content) === 'PHP') {
             // Upload code to hastebin.com
-            
 
-
-            hastebin.createPaste(message.content, {
-                raw: true,
-                contentType: 'text/plain',
-                server: 'https://hastebin.com'
-              }).then(haste => {
+            hastebin(message.content).then(haste => {
                 var fixedcode = "```" + `${detectLang(message.content).toLowerCase()}` + `\n${message.content}` + '```';
                 message.channel.send(fixedcode);
                 message.channel.send(`From ${message.author.toString()}, ` + 'a web version is available: ' + haste);
