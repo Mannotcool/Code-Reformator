92dd89e84eee99cd7d2acce6e8aae6d81ef647d3
Nick <nickstoyanov2019@gmail.com>
2022-01-04 14:00:32 -0500

    Fix hastebin script

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const { Client } = require('discord.js');
 const client = new Client({ intents: 32509 });
 const config = require('./config.json');
-const hastebin = require("hastebin-paste");
+const hastebin = require("hastebin");
 var detectLang = require('lang-detector');
 
 client.on('ready', async () => {
@@ -9,14 +9,21 @@ client.on('ready', async () => {
 });
 client.on("messageCreate", async (message) => {
     if(message.author.id === client.user.id) return;
-    if(message.mentions.everyone || message.mentions.here) return;
+        // detect if message contains @everyone or @here and return if true
+        if(message.mentions.everyone || message.mentions.here) return;
         if (detectLang(message.content) === 'JavaScript' || detectLang(message.content) === 'C' || detectLang(message.content) === 'C++' || detectLang(message.content) === 'Python' || detectLang(message.content) === 'Java' || detectLang(message.content) === 'HTML' || detectLang(message.content) === 'CSS' || detectLang(message.content) === 'Ruby' || detectLang(message.content) === 'Go' || detectLang(message.content) === 'PHP') {
             // Upload code to hastebin.com
             
-            hastebin(message.content).then(haste => {
+
+
+            hastebin.createPaste(message.content, {
+                raw: true,
+                contentType: 'text/plain',
+                server: 'https://hastebin.com'
+              }).then(haste => {
                 var fixedcode = "```" + `${detectLang(message.content).toLowerCase()}` + `\n${message.content}` + '```';
                 message.channel.send(fixedcode);
-                message.channel.send(`From ${message.author.toString()}, ` + 'a web version is available:' + haste.slice(13, -41));
+                message.channel.send(`From ${message.author.toString()}, ` + 'a web version is available: ' + haste);
                 message.delete();
             }).catch(error => {
                 // Handle error
